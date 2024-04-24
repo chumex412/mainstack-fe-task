6b419df6311c0c52763e4023f22db8382449b04f
Kode Navigator <chumex412@gmail.com>
2024-04-24 09:09:35 +0100

    modifies primary button to accept other paint styles

--- src/components/ui/primary-button/index.tsx
+++ src/components/ui/primary-button/index.tsx
@@ -6,7 +6,7 @@ type ButtonType = ButtonProps & ComponentPropsWithoutRef<"button">;
 const PrimaryButton = ({ customClass, value, ...props }: ButtonType) => {
   return (
     <button
-      className={`leading-ll rounded-[100px] border px-6 py-3 text-base font-semibold text-white transition-colors duration-300 ${customClass}`}
+      className={`rounded-[100px] border px-6 py-3 text-base font-semibold leading-ll transition-colors duration-300 ${customClass}`}
       {...props}
     >
       {value}
