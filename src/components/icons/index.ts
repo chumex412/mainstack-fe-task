49620b0b723430a3df253fc2093dd8af6859116d
Kode Navigator <chumex412@gmail.com>
2024-04-24 18:42:10 +0100

    styles active link s

---
+++ src/components/icons/index.ts
@@ -0,0 +1,7 @@
+import GroupIcon from "./GroupIcon";
+import HomeIcon from "./HomeIcon";
+import InsertIcon from "./InsertIcon";
+import PaymentIcon from "./PaymentIcon";
+import WidgetIcon from "./WidgetIcon";
+
+export { GroupIcon, HomeIcon, InsertIcon, PaymentIcon, WidgetIcon };
--- src/components/ui/nav-bar/NavLInks.tsx
+++ src/components/ui/nav-bar/NavLInks.tsx
@@ -1,27 +1,34 @@
 "use client";
 
-import { NavLinksProps } from "@/application/domain/entities/ui";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { FC, SVGProps } from "react";
+import { IconProps, NavLinksProps } from "@/application/domain/entities/ui";
 
-const NavLinks = ({ links = [] }: NavLinksProps) => {
+const NavLinks = ({
+  links = []
+}: NavLinksProps<FC<SVGProps<SVGSVGElement>>>) => {
   const currentPath = usePathname();
   return (
     <ul className="flex items-center gap-5">
-      {links.map((link) => (
-        <li
-          key={link.name}
-          className={`${currentPath === link.path ? "active" : ""} p-2`}
-        >
-          <Link className="flex items-center gap-1" href={link.path}>
-            <Image src={link.icon} alt={link.name} />
-            <span className="leading-ll text-black-300 text-base capitalize">
-              {link.name}
-            </span>
-          </Link>
-        </li>
-      ))}
+      {links.map((link) => {
+        return (
+          <li
+            key={link.name}
+            className={`${currentPath === link.path ? "active" : ""} p-2`}
+          >
+            <Link className="flex items-center gap-1" href={link.path}>
+              <link.Icon fill="#000" />
+              <span
+                className={`text-base capitalize leading-ll ${currentPath === link.path ? "text-white" : "text-black-300"}`}
+              >
+                {link.name}
+              </span>
+            </Link>
+          </li>
+        );
+      })}
     </ul>
   );
 };
