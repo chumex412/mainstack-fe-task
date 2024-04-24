adaec317f7960662eabf66642c2bca446d9964f7
Kode Navigator <chumex412@gmail.com>
2024-04-24 18:52:16 +0100

    sets a default for payout chart

--- src/components/containers/PayoutChartContainer.tsx
+++ src/components/containers/PayoutChartContainer.tsx
@@ -6,7 +6,13 @@ import { PayoutChart } from "../pages/revenue";
 const PayoutChartContainer = () => {
   const { txns } = useTransaction();
 
-  return <PayoutChart data={txns} />;
+  return (
+    <PayoutChart
+      data={
+        txns.length ? txns : [{ timestamp: "", amount: 0, title: "", type: "" }]
+      }
+    />
+  );
 };
 
 export default PayoutChartContainer;
--- src/components/pages/revenue/Payout.tsx
+++ src/components/pages/revenue/Payout.tsx
@@ -2,10 +2,12 @@ import {
   PayoutChartContainer,
   PayoutDetailsContainer
 } from "@/components/containers";
+import AppBar from "./AppBar";
 
 const Payout = () => {
   return (
     <section className="container flex flex-col gap-5 py-16 lg:flex-row">
+      <AppBar />
       <section className="payout-main">
         <PayoutChartContainer />
         <PayoutDetailsContainer />
