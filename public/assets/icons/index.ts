189ceb340ece800da6406ca1b60504ffa2f30ead
Kode Navigator <chumex412@gmail.com>
2024-04-24 18:23:16 +0100

    adds icons

--- public/assets/icons/index.ts
+++ public/assets/icons/index.ts
@@ -1,16 +1,17 @@
-import callMadeIcon from "./call_made.svg";
-import callRecvIcon from "./call_received.svg";
-import chatsIcon from "./chats.svg";
-import closeIcon from "./close.svg";
-import downloadIcon from "./download.svg";
-import expandIcon from "./expand.svg";
-import groupIcon from "./group.svg";
+import callMadeIcon from "./call_made.svg?url";
+import callRecvIcon from "./call_received.svg?url";
+import chatsIcon from "./chats.svg?url";
+import closeIcon from "./close.svg?url";
+import downloadIcon from "./download.svg?url";
+import emptyFileIcon from "./empty_file.svg?url";
+import expandIcon from "./expand.svg?url";
+import groupIcon from "./group.svg?url";
 import homeIcon from "./home.svg";
-import inserChartIcon from "./insert_chart.svg";
-import logo from "./logo.svg";
-import menuIcon from "./menu.svg";
-import notificationIcon from "./notification.svg";
-import widgetIcon from "./widget.svg";
+import inserChartIcon from "./insert_chart.svg?url";
+import logo from "./logo.svg?url";
+import menuIcon from "./menu.svg?url";
+import notificationIcon from "./notification.svg?url";
+import widgetIcon from "./widget.svg?url";
 
 export {
   callMadeIcon,
@@ -18,6 +19,7 @@ export {
   chatsIcon,
   closeIcon,
   downloadIcon,
+  emptyFileIcon,
   expandIcon,
   groupIcon,
   homeIcon,
